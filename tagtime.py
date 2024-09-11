59a89b7fc527e75a1c1def68671dcc0a8f1268e9
VenusMods <cortrocket@gmail.com>
2024-09-11 04:25:06 -0700

    Update tagtime.py

--- tagtime.py
+++ tagtime.py
@@ -13,31 +13,25 @@ import os
 import prompt
 import settings
 import logviewer
-import sys
 
-def resource_path(relative_path):
-    try:
-        base_path = sys._MEIPASS2
-    except Exception:
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, relative_path)
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.realpath(__file__))
 
 def str_to_bool(value):
   return value.lower() in ("yes", "true", "t", "1")
 
 def on_config_save(value):
         config['Settings']['last_ping'] = value
-        with open(resource_path('config.ini'), 'w') as configfile:
+        with open(os.path.join(script_dir, 'config.ini'), 'w') as configfile:
             config.write(configfile)
 
 def on_config_save_first_time(value):
         config['Settings']['first_time'] = value
-        with open(resource_path('config.ini'), 'w') as configfile:
+        with open(os.path.join(script_dir, 'config.ini'), 'w') as configfile:
             config.write(configfile)
 
 config = configparser.ConfigParser()
-config.read(resource_path('config.ini'))
+config.read(os.path.join(script_dir, 'config.ini'))
 tagtime_start = int(config['Settings']['urping']) # ur-ping, start of tagtime
 seed = int(config['Settings']['seed']) # the seed
 gap = int(config['Settings']['gap']) # Average gap in minutes
@@ -55,43 +49,57 @@ async def run_tagtime():
 
 # Function to generate the next ping time based on the exponential distribution
 def next_ping_time(last_ping_time, gap):
+    # print(last_ping_time, " last ping time")
     gap = int(config['Settings']['gap'])   
     wait_time_minutes = -gap * math.log(1 - random.random())
     # print("wait time minutes", wait_time_minutes)
     wait_time = timedelta(minutes=wait_time_minutes)
-    # print(wait_time.total_seconds(), " wait time")
-    # print(last_ping_time + wait_time.total_seconds(), " last ping + wait time")
-    return last_ping_time + wait_time.total_seconds(), wait_time.total_seconds()
+    # print(int(wait_time.total_seconds()), " wait time")
+    new_ping_time = int(last_ping_time + wait_time.total_seconds())
+    # print(new_ping_time)
+    return new_ping_time
 
 async def first_time_check(now, last_ping_time, gap):
     count = 0
     gap = int(config['Settings']['gap'])
     print("Starting first time check. Login to the Cloud in Settings To Sync Your Logs.")
+    print("first time check: ", now, " now, ", last_ping_time, " last ping time")
     while (now > last_ping_time):
-        last_ping_time, wait_time_seconds = next_ping_time(last_ping_time, gap)
+        last_ping_time = next_ping_time(last_ping_time, gap)
         count += 1
     print(f"Skipped over {count} prompts!")
-    return last_ping_time, wait_time_seconds
+    return last_ping_time
 
-async def catch_up(now, last_ping_time, gap):
+async def catch_up(now, start_time, last_ping_time, gap):
     count = 0
     gap = int(config['Settings']['gap'])
     print("Catching up since last ping...")
-    log_file_path = resource_path("log.log")  # Define log file path
-    while (now > last_ping_time):
-        last_ping_time, wait_time_seconds = next_ping_time(last_ping_time, gap)
+    log_file_path = os.path.join(script_dir, "log.log")  # Define log file path
+
+    print(start_time, " initial start time")
+    print(last_ping_time, " initial last ping time")
+
+    while (last_ping_time > start_time):
+        start_time = next_ping_time(start_time, gap)
         count += 1
+
+    count = 0
+
+    print(now, " now")
+    print(start_time, " start time")
+
+    while (now > start_time):
         # Get the current timestamp
-        last_ping_time_unix = int(last_ping_time)
+        new_ping_time_unix = int(start_time)
 
         # Convert UNIX timestamp to a datetime object
-        last_ping_datetime = datetime.fromtimestamp(last_ping_time_unix)
+        new_ping_datetime = datetime.fromtimestamp(new_ping_time_unix)
 
         # Format the datetime object as desired
-        formatted_last_ping_time = last_ping_datetime.strftime("%Y.%m.%d %H:%M:%S")
+        formatted_last_ping_time = new_ping_datetime.strftime("%Y.%m.%d %H:%M:%S")
 
         # Get the day abbreviation
-        day = last_ping_datetime.strftime("%a").upper()
+        day = new_ping_datetime.strftime("%a").upper()
         day_abbr = day[:3].upper()
 
         # format tags
@@ -99,11 +107,14 @@ async def catch_up(now, last_ping_time, gap):
         formatted_tags = (tags[:50]).ljust(50)
 
         # Format the log entry
-        log_entry = f'{last_ping_time_unix} {formatted_tags} [{formatted_last_ping_time} {day_abbr}]\n'
+        log_entry = f'{new_ping_time_unix} {formatted_tags} [{formatted_last_ping_time} {day_abbr}]\n'
+
+        start_time = next_ping_time(start_time, gap)
+        count += 1
         
         now = int(time.time())
 
-        if last_ping_time_unix < now:
+        if new_ping_time_unix < now:
             # Write to the log file
             with open(log_file_path, "a") as log_file:
                 log_file.write(log_entry)
@@ -111,19 +122,21 @@ async def catch_up(now, last_ping_time, gap):
             count -= 1
 
     print(f"you missed {count} pings!")
-    return last_ping_time, wait_time_seconds
+    return start_time
 
 async def loop_time(new_ping_time, now):
     while (now < new_ping_time):
         await asyncio.sleep(10)
         now = int(time.time())
+        print(int(new_ping_time - now), " seconds left")
 
 def show_info_message(title, message):
+    img_path = os.path.join(script_dir, "img")
     notification.notify(
         title=title,
         message=message,
         app_name="TagTime",
-        app_icon=resource_path('img\\tagtime.ico')
+        app_icon=os.path.join(img_path, 'tagtime.ico')
     )
 
 
@@ -132,32 +145,30 @@ async def tagtime_pings(start_time, gap):
     now = int(time.time())
     gap = int(config['Settings']['gap'])
     if (first_time):
-        last_ping_time = start_time
-        last_ping_time, wait_time_seconds = await first_time_check(now, last_ping_time, gap)
-        new_ping_time = last_ping_time + wait_time_seconds
-        on_config_save(str(int(last_ping_time)))
+        new_ping_time = await first_time_check(now, start_time, gap)
+        print(now, " now")
+        print(new_ping_time, " new ping")
+        on_config_save(str(int(new_ping_time)))
         on_config_save_first_time("False")
         show_info_message("Success!", "TagTime Successfully Installed!\nTagTime will now ping you randomly! If you want to log in, go to settings.")
         loop = asyncio.get_event_loop()
         asyncio.run_coroutine_threadsafe(run_tagtime(), loop)
     else:
-        last_ping_time = int(config['Settings']['last_ping'])
-        if now > last_ping_time:
-            last_ping_time, wait_time_seconds = await catch_up(now, last_ping_time, gap)
-            new_ping_time = last_ping_time + wait_time_seconds
-            on_config_save(str(int(last_ping_time)))
+        new_ping_time = int(config['Settings']['last_ping'])
+        if now > new_ping_time:
+            new_ping_time = await catch_up(now, start_time, new_ping_time, gap)
+            on_config_save(str(int(new_ping_time)))
     while True:
-        last_ping_time, wait_time_seconds = next_ping_time(last_ping_time, gap)
-        new_ping_time = last_ping_time + wait_time_seconds
-        if new_ping_time > last_ping_time:
-            now = int(time.time())
-            final_wait_time = int(new_ping_time) - now
-            print(f"Next ping at {int(new_ping_time)}, which is in {final_wait_time:.2f} seconds.")
-            await loop_time(new_ping_time, now)
+        gap = int(config['Settings']['gap'])
+        now = int(time.time())
+        final_wait_time = int(new_ping_time) - now
+        print(f"Next ping at {int(new_ping_time)}, which is in {final_wait_time:.2f} seconds.")
+        await loop_time(new_ping_time, now)
         print(f"Ping at {int(new_ping_time)}")
         on_config_save(str(int(new_ping_time)))
         loop = asyncio.get_event_loop()
         asyncio.run_coroutine_threadsafe(run_tagtime(), loop)
+        new_ping_time = next_ping_time(new_ping_time, gap)
 
 def run_settings():
     try:
@@ -178,7 +189,8 @@ def destroy(trayicon):
     os._exit(0)   # Exit the program
 
 async def create_tray_icon():
-    image = Image.open(resource_path('img\\tagtime.ico'))
+    img_path = os.path.join(script_dir, "img")
+    image = Image.open(os.path.join(img_path, 'tagtime.ico'))
     trayicon = pystray.Icon("Tagtime", image, menu=pystray.Menu(
         pystray.MenuItem("Settings", run_settings),
         pystray.MenuItem("Log Viewer/Editor", run_logviewer),
